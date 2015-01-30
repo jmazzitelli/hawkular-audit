fd1e32f49f0e2e317d7d24df98c9e1268c53fe76
John Mazzitelli <mazz@redhat.com>
2015-01-30 18:36:08 -0500

    reformat/license

--- hawkular-audit-api/src/main/java/org/hawkular/audit/common/AuditRecord.java
+++ hawkular-audit-api/src/main/java/org/hawkular/audit/common/AuditRecord.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hawkular.audit.common;
 
 import java.util.Map;
@@ -9,10 +25,10 @@ import com.google.gson.annotations.Expose;
 
 /**
  * Information about an audited event.
- * 
+ *
  * A {@link #getSubsystem() subsystem} identifies in which subsystem the event occurred. If not specified,
  * {@link Subsystem#MISCELLANEOUS} is used.
- * 
+ *
  * The {@link #getTimestamp() timestamp} identifies when the event occurred. If not specified, the current time is used.
  * Note this is not representative of when this AuditRecord was sent to the backend store. This is supposed to identify
  * when the actual audited event happened.
--- hawkular-audit-api/src/test/java/org/hawkular/audit/consumer/AuditRecordConsumerTest.java
+++ hawkular-audit-api/src/test/java/org/hawkular/audit/consumer/AuditRecordConsumerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hawkular.audit.consumer;
 
 import java.util.ArrayList;
@@ -18,8 +34,8 @@ public class AuditRecordConsumerTest extends ProducerConsumerSetup {
         final int numberOfTestRecords = 5;
         final CountDownLatch latch = new CountDownLatch(numberOfTestRecords);
         final ArrayList<AuditRecord> records = new ArrayList<AuditRecord>();
-        final StoreAndLatchBasicMessageListener<AuditRecord> listener = new StoreAndLatchBasicMessageListener<AuditRecord>(latch, records, null,
-                AuditRecord.class);
+        final StoreAndLatchBasicMessageListener<AuditRecord> listener = new StoreAndLatchBasicMessageListener<AuditRecord>(
+                latch, records, null, AuditRecord.class);
 
         consumer.listen(listener);
 
@@ -47,8 +63,8 @@ public class AuditRecordConsumerTest extends ProducerConsumerSetup {
         final int numberOfTestRecords = 5;
         final CountDownLatch latch = new CountDownLatch(numberOfTestRecords);
         final ArrayList<AuditRecord> records = new ArrayList<AuditRecord>();
-        final StoreAndLatchBasicMessageListener<AuditRecord> listener = new StoreAndLatchBasicMessageListener<AuditRecord>(latch, records, null,
-                AuditRecord.class);
+        final StoreAndLatchBasicMessageListener<AuditRecord> listener = new StoreAndLatchBasicMessageListener<AuditRecord>(
+                latch, records, null, AuditRecord.class);
 
         consumer.listen(listener);
 
